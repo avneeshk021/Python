72cee9e8cccdf4135246a713ea847f510ef1b3a0
Avneesh Kumar <kaviikumar010@gmail.com>
2023-06-15 13:01:41 +0530

    new

---
+++ forloop.py
@@ -0,0 +1,8 @@
+# x=int(input("Enter the Number"))
+print("values of X")
+for x in range(6):
+    print(x)
+# y=int(input("Enter the Number"))
+print("Values of Y ")
+for y in range(1,11):
+    print(y) 
