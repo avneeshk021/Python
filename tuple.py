56a23f46696d7c9bc1d175da4c8c1ea40d8f6fb1
Avneesh Kumar <kaviikumar010@gmail.com>
2023-06-20 13:09:47 +0530

    day3

--- tuple.py
+++ tuple.py
@@ -1,4 +1,5 @@
 tuple=('A','B','C','D','E','F')
+
 # print(tuple)
 # print(type(tuple))
 # print(len(tuple))
@@ -6,8 +7,17 @@ tuple=('A','B','C','D','E','F')
 # tuple='0'
 # print(tuple)
 temp=list(tuple)
-temp.append("hi")
-print(temp)
+# temp.append("hi")
+# print(temp)
+# temp.remove('A')
+# print(temp)
+# temp.pop(0)
+# print(temp)
+# # temp.clear()
+# print(temp)
+for i in temp:
+    print(i)
+
 
 
 
