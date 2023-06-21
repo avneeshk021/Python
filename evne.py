be3c694261218d28f2ef2b249c6db0f90f9ea102
Avneesh Kumar <kaviikumar010@gmail.com>
2023-06-21 13:11:37 +0530

    new one

--- evne.py
+++ evne.py
@@ -1,11 +1,11 @@
 n=int(input("Enter the Number ="))
 # for i in range(1,n):
 #     if(i%2==0):
-#         print(i)
+#         print("Even")
 i=0
 while(n>0):
    if(i%2==0):
-        print(i)
+        print("Even")
         i+=1
 
     
---
+++ stringg.py
@@ -0,0 +1,18 @@
+txt="HI", "Everyone"
+
+print(txt)
+print(type(txt))
+#Here we use slicing
+print(txt[:1])
+print(txt[1:])
+#here we use loop
+for i in txt:
+    print(i)
+    #here we use index value
+print(txt[0])
+ #here we use add two string
+print("Hi"+" "+"Everyone")
+
+
+
+
