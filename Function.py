608eab2413b20973ebe739bf027fe88e2ba172ed
Avneesh Kumar <kaviikumar010@gmail.com>
2023-06-22 12:07:21 +0530

    new commit

---
+++ Function.py
@@ -0,0 +1,35 @@
+
+# def sum():
+#     a=int(input("Enter fisrt Number ="))
+#     b=int(input("Enter second number"))
+#     print("sum",a+b)
+#     print("sub",a*b)
+#     print("Divison",a/b)
+#     print("Mudulo",a%b)
+# sum()
+# def user(yourname,lastname):
+#     print("Hello"+" "+yourname+" "+lastname)
+# user("Avneesh","Kumar")
+
+# def fun(yourname,lastname):
+#     print("Hello"+" "+yourname+" "+lastname)
+# user(lastname="bhatt",yourname="Aaliya")
+
+# def marvals(iron,thander):
+#     print("ironman",iron, "Thor",thander)
+# marvals(iron="Avengers",thander="thnderstrom")
+
+def add(x):
+   return 10*x
+print(add(6))
+
+
+    
+
+
+
+
+
+
+
+
