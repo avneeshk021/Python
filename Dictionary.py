c85424d17468759ed9df7d815a48458a9ba06f36
Avneesh Kumar <kaviikumar010@gmail.com>
2023-06-22 12:12:39 +0530

    new

--- Dictionary.py
+++ Dictionary.py
@@ -1,12 +1,13 @@
 dic={"Name": 'Avii',
      "Roll No.": 20   
 }
-print(dic)
-# print(type(dic))
-# print(len(dic))
-print(dic["Name"])
-print(dic["Roll No."])
-for i in dic:
-    print(i)
-dic.copy()
-print(dic)
+# print(dic)
+# # print(type(dic))
+# # print(len(dic))
+# print(dic["Name"])
+# print(dic["Roll No."])
+# for i in dic:
+#     print(i)
+# dic.copy()
+# print(dic)
+print(dic["Name" "Avneesh"])
